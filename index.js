3cac56e5e1165c81f7ebc306ca524eb8b02e6313
Arathy06 <arathysree97@gmail.com>
2020-11-10 00:44:28 +0530

    api response completed

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const e = require('express');
+var express = require('express')
+
+var app = express()
+
+app.get("/",(req,res)=>{
+
+    res.send("Welcome to my website")
+})
+
+app.get("/contact",(req,res)=>{
+
+    res.send("Welcome to my contact page")
+})
+
+
+app.get("/home",(req,res)=>{
+
+    res.send("Welcome to my home page")
+})
+
+app.get("/gallery",(req,res)=>{
+
+    res.send("Here is my gallery")
+})
+
+app.get("/profile",(req,res)=>{
+
+    res.send("My profile page")
+})
+
+
+app.listen(3000,(error)=>{
+    if(error)
+    {
+        console.log("Error occured")
+    }
+    else
+    {
+        console.log("Server started")
+    }
+
+});
